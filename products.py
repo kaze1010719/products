c9c1807e523c1c7bb828a3c62d60e8ceaef2f83e
Louise <kaze1010719@gmail.com>
2022-06-15 22:17:12 +0800

    Add products.py

---
+++ products.py
@@ -0,0 +1,22 @@
+# while 適合用在不知有幾次的情況
+products = []
+while True:
+	name = input('請輸入商品名稱: ')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格: ')
+
+	#方法一
+	#p = []
+	#p.append(name)
+	#p.append(price)
+	#products.append(p)
+
+	#方法二
+	#p = [name, peice]
+	#products.append(p)
+
+	#方法三
+	products.append([name, price])
+
+print(products)
